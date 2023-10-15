efa18c0bd87a2cd1cda55ca9c2457376c5922c00
uzunvictor <v.lungu@webdelo.org>
2023-10-15 20:58:26 +0300

    created ProductImage.vue

---
+++ src/shared/components/ProductImage.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+.image
+  span.font_icon(
+    :class="name || ''"
+  )
+</template>
+
+<script setup lang="ts">
+import {computed} from "vue";
+import type {Image} from "@/shared/constants/image";
+import type {Color} from "@/shared/constants/color";
+
+// ------------------- props --------------------------->
+interface Props {
+  color : Color
+  name  : Image
+}
+const props = defineProps<Props>()
+
+// ------------------- computed ------------------------>
+const color = computed(() => props.color || '$text_grey_light')
+</script>
+
+<style lang="scss" scoped>
+.image {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    span {
+      &.font_icon{
+        &:before{
+          color: v-bind(color);
+          font-size: 224px;
+        }
+      }
+    }
+}
+</style>
