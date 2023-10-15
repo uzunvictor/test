e8630cefc3b147079c99fccda362ddb00b36a511
uzunvictor <v.lungu@webdelo.org>
2023-10-15 21:01:38 +0300

    edit of PageHome by adding suspense component as a wrapper for products grid, added styles.

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,13 +1,20 @@
 <template lang="pug">
 .page-container
+  Suspense
+    template(#fallback) Loading...
+    ProductsGrid
 </template>
 
 <script setup lang="ts">
+import {defineAsyncComponent} from "vue";
 
+// ------------------- components ---------------------->
+const ProductsGrid = defineAsyncComponent(() => import('@/shared/components/ProductsGrid.vue'))
 </script>
 
 <style lang="scss" scoped>
 .page-container {
-  padding: 32px;
+  border-radius: 24px;
+  overflow: hidden;
 }
 </style>
