be984262322ad2d1584e11a597d74273015e18ad
uzunvictor <v.lungu@webdelo.org>
2023-10-15 23:39:37 +0300

    changed the App.vue template structure

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
+<template lang="pug">
+header
+  TheHeader
+RouterView
+</template>
+
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import TheHeader from "@/shared/components/TheHeader.vue";
+
 </script>
-
-<template>
-  <header>
-  </header>
-
-  <RouterView />
-</template>
 
 <style lang="scss" scoped>
 
