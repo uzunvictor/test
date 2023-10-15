b847f0127e4e4aa25416c9fa11d2cbc123b047c2
uzunvictor <v.lungu@webdelo.org>
2023-10-15 18:41:08 +0300

    set main styles for application

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,13 @@ import VitePugPlugin from 'vite-plugin-pug';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/assets/scss/_variables-color.scss";`
+      }
+    }
+  },
   plugins: [
     vue(),
     VitePugPlugin(),
