d9ab08d760595b8f54b9131bc7117fb4ff4b15e9
uzunvictor <v.lungu@webdelo.org>
2023-10-15 20:59:10 +0300

    added bgColor and color fields for Product type

--- src/shared/types/products.ts
+++ src/shared/types/products.ts
@@ -1,8 +1,11 @@
 import type {Image} from "@/shared/constants/image";
+import type {Color} from "@/shared/constants/color";
 
 export type Product = {
   id           : number
-  description ?: string
-  image       ?: Image
-  title       ?: string
+  bgColor      : Color
+  color        : Color
+  description  : string
+  image        : Image
+  title        : string
 }
