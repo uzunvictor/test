8c219211602dcc9f3d91d00cd261fdd95d1d431c
uzunvictor <v.lungu@webdelo.org>
2023-10-16 14:44:18 +0300

    added FeedbackForm to home page

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,20 +1,35 @@
 <template lang="pug">
-.page-container
-  Suspense
-    template(#fallback) Loading...
+.page-home
+  Suspense(v-if="activeTab === 'Products' || isDesktop")
+    template(#fallback)
+      LoaderCircle(center :color="Color.FLORAL_GREEN_LIGHT")
     ProductsGrid
+  FeedbackForm(v-if="activeTab === 'Feedback' || isDesktop")
 </template>
 
 <script setup lang="ts">
-import {defineAsyncComponent} from "vue";
+import {storeToRefs} from "pinia";
+import {computed, defineAsyncComponent, ref} from "vue";
+import {Color} from "@/shared/constants/color";
+import {useStoreHeader} from "@/stores/header";
+import LoaderCircle from "@/shared/components/LoaderCircle.vue";
+
+// ------------------- state --------------------------->
+const { activeTab, isDesktop } = storeToRefs(useStoreHeader())
 
 // ------------------- components ---------------------->
 const ProductsGrid = defineAsyncComponent(() => import('@/shared/components/ProductsGrid.vue'))
+const FeedbackForm = defineAsyncComponent(() => import('@/shared/components/FeedbackForm.vue'))
 </script>
 
 <style lang="scss" scoped>
-.page-container {
-  border-radius: 24px;
-  overflow: hidden;
+.page-home {
+  min-height: calc(100vh - 160px);
+  display: flex;
+
+  @media (max-width: 1439.99px) {
+    flex-direction: column;
+    align-items: center;
+  }
 }
 </style>
