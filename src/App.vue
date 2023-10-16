e18844c8abeedbb8b8962753579984fbcd371d96
uzunvictor <v.lungu@webdelo.org>
2023-10-16 14:42:22 +0300

    changes of body and #app tags height styles

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template lang="pug">
-header
-  TheHeader
-RouterView
+.main-layout(@resize="setIsDesktop")
+  header
+    TheHeader
+  RouterView
 </template>
 
 <script setup lang="ts">
+import {onMounted} from "vue";
+import {useStoreHeader} from "@/stores/header";
 import { RouterLink, RouterView } from 'vue-router'
 import TheHeader from "@/shared/components/TheHeader.vue";
+
+// ------------------- functions ----------------------->
+const { setIsDesktop } = useStoreHeader()
+
+// ------------------- hooks ---------------------------->
+onMounted(() => {
+  setIsDesktop()
+  window.addEventListener('resize', setIsDesktop)
+})
 
 </script>
 
 <style lang="scss" scoped>
-
+.main-layout {
+  min-height: calc(100vh - 96px);
+}
 </style>
